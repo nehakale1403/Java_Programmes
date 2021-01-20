00d0da52cb3ebced10f8435d3e975e352f02ce5c
Neha Kale <nkale8013@gmail.com>
2021-01-20 11:49:31 +0530

    basic r=programmes of addition

---
+++ data_types/src/com/nehak/Main.java
@@ -0,0 +1,20 @@
+package com.nehak;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        byte age = 21;
+        int age2 = 48;
+        short age3 = 98;
+        long ageDino = 1928288882L;
+        char ch = 'A';
+        float f1 = 5.6f; //if we do not specify f ...then by default it will take double value
+        double d1 = 4.99;
+        boolean a = true;
+        String str = "Neha";
+
+        System.out.print(str);
+
+    }
+}
---
+++ sum/src/com/nehak/Main.java
@@ -0,0 +1,13 @@
+package com.nehak;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int num1 = 1;
+        int num2 = 2;
+        int num3 = 3;
+        int sum = num1+num2+num3;
+        System.out.print(sum);
+    }
+}
