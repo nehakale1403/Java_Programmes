e50fe648f25d58993363d7460fc931353484b6d4
Neha Kale <nkale8013@gmail.com>
2021-01-22 13:49:16 +0530

    practice set question2

---
+++ sum/src/com/nehak/java_12_practice2.java
@@ -0,0 +1,14 @@
+package com.nehak;
+
+public class java_12_practice2 {
+    public static void main(String[] args) {
+        //encrypt and decrypt
+
+        char grade = 'B';
+        grade = (char) (grade+8);
+        System.out.println(grade);
+
+        grade = (char) (grade-8);
+        System.out.println(grade);
+    }
+}
