7135cb1d5fe4adadb1cf83fe7d12d8d982173c05
Neha Kale <nkale8013@gmail.com>
2021-01-22 13:44:54 +0530

    practice set question1

---
+++ sum/src/com/nehak/java_12_practice.java
@@ -0,0 +1,13 @@
+package com.nehak;
+
+public class java_12_practice {
+    public static void main(String[] args) {
+
+        //Question 1
+        float a = 7/4 * 9/2; //it is evaluated as int and converted to float
+        System.out.println(a);
+        float b = 7/4.0f * 9/2.0f; //correct method
+        System.out.println(b);
+
+    }
+}
