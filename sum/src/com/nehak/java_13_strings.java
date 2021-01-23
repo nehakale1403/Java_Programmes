199a6ee2dc9156c96f4fb51cc7f377c517965d29
Neha Kale <nkale8013@gmail.com>
2021-01-23 22:48:49 +0530

    strings

---
+++ sum/src/com/nehak/java_13_strings.java
@@ -0,0 +1,15 @@
+package com.nehak;
+
+public class java_13_strings {
+    public static void main(String[] args) {
+
+        String name = new String("Neha");
+        System.out.println(name);
+
+        //string is a class bt can be used as a data type
+        String n = "Neha"; // n is reference and Neha is an object
+        System.out.println(n);
+        //Strings are immutable
+
+    }
+}
